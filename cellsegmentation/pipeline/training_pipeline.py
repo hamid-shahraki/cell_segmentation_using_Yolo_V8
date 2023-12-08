04b2101e2d1b5b0eadd03d2a696ceee107ba6e57
hamid shahraki <hamidshahraki@wl-r6-203.rz.tu-ilmenau.de>
2023-12-08 12:58:35 +0100

    implementation added

--- cellsegmentation/pipeline/training_pipeline.py
+++ cellsegmentation/pipeline/training_pipeline.py
@@ -3,18 +3,20 @@ from cellsegmentation.logger import logging
 from cellsegmentation.exception import AppException
 from cellsegmentation.components.data_ingestion import DataIngestion
 from cellsegmentation.components.data_validation import DataValidation
+from cellsegmentation.components.model_trainer import ModelTrainer
 
-
-from cellsegmentation.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from cellsegmentation.entity.config_entity import DataIngestionConfig, DataValidationConfig, ModelTrainerConfig
                                               
 
-from cellsegmentation.entity.artifacts_entity import DataIngestionArtifact, DataValidationArtifact
+from cellsegmentation.entity.artifacts_entity import DataIngestionArtifact, DataValidationArtifact, ModelTrainerArtifact
                                                    
 
 class TrainPipeline:
+
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_validation_config = DataValidationConfig()
+        self.model_trainer_config = ModelTrainerConfig()
 
     
     def start_data_ingestion(self)-> DataIngestionArtifact:
@@ -63,6 +65,21 @@ class TrainPipeline:
         except Exception as e:
             raise AppException(e, sys) from e
         
+    
+    def start_model_trainer(self
+    ) -> ModelTrainerArtifact:
+        try:
+            model_trainer = ModelTrainer(
+                model_trainer_config=self.model_trainer_config
+            )
+            model_trainer_artifact = model_trainer.initiate_model_trainer()
+            return model_trainer_artifact
+
+        except Exception as e:
+            raise AppException(e, sys)
+        
+
+        
     def run_pipeline(self) -> None:
 
         try: 
@@ -70,6 +87,12 @@ class TrainPipeline:
             data_validation_artifact = self.start_data_validation(
                 data_ingestion_artifact=data_ingestion_artifact
             )
+
+            if data_validation_artifact.validation_status == True:
+                model_trainer_artifact = self.start_model_trainer()
+            
+            else:
+                raise Exception("Your data is not in correct format")
 
         except Exception as e:
             raise AppException(e, sys)
